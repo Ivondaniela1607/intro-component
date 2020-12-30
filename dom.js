655d05ffd41eb492f9fb8e22e918865d33e4b7bf
IvonneDaniela <ivondaniela1607@gmail.com>
2020-12-30 15:46:06 -0500

    change detail

--- dom.js
+++ dom.js
@@ -3,8 +3,14 @@ function view(){
   document.getElementById('msj2').style.display="block";
   document.getElementById('msj3').style.display="block";
   document.getElementById('msj4').style.display="block";
+
   document.getElementById('cInput').style.border = "thin solid #ff5263"
   document.getElementById('cInput1').style.border = "thin solid #ff5263"
   document.getElementById('cInput2').style.border = "thin solid #ff5263"
   document.getElementById('cInput3').style.border = "thin solid #ff5263"
+
+  document.getElementById('icon').style.display="block";
+  document.getElementById('icon1').style.display="block";
+  document.getElementById('icon2').style.display="block";
+  document.getElementById('icon3').style.display="block";
 }
