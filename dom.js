eb5b699756b4cad3a50697e7622454c8a01482ed
IvonneDaniela <ivondaniela1607@gmail.com>
2020-12-29 23:29:19 -0500

    finish style

---
+++ dom.js
@@ -0,0 +1,10 @@
+function view(){
+  document.getElementById('msj1').style.display="block";
+  document.getElementById('msj2').style.display="block";
+  document.getElementById('msj3').style.display="block";
+  document.getElementById('msj4').style.display="block";
+  document.getElementById('cInput').style.border = "thin solid #ff5263"
+  document.getElementById('cInput1').style.border = "thin solid #ff5263"
+  document.getElementById('cInput2').style.border = "thin solid #ff5263"
+  document.getElementById('cInput3').style.border = "thin solid #ff5263"
+}
